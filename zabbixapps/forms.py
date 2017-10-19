5e9e801544aceda3ba6a9c263d40ba5d7251f2b5
tjdyh <bailiguang2009@163.com>
2017-10-19 14:21:46 +0800

    修改主页模板

--- zabbixapps/forms.py
+++ zabbixapps/forms.py
@@ -6,6 +6,8 @@ class CreateHostAddForm(forms.Form):
     host = Zabbixtools()
     # groupid_choices = [('2', 'Linux Server'), ('10', 'testing')]
     # templateid_choices = [('10001', 'Template OS Linux'), ('10050', 'Template App Zabbix Agent')]
+    groupid_choices = []
+    templateid_choices = []
     groupid_choices = host.hostgroup_get()
     templateid_choices = host.template_get()
     # pprint(templateid_choices)
