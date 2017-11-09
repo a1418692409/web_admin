be5edcfe0325466945b6b967c37402fae269b5bb
tjdyh <bailiguang2009@163.com>
2017-11-09 19:01:32 +0800

    修改

--- zabbixapps/models.py
+++ zabbixapps/models.py
@@ -8,3 +8,5 @@ class Article(models.Model):
     content = models.TextField(u'内容')
     pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
     update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+    def __unicode__(self):
+        return self.title
